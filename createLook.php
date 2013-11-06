9b40a30059d67e662da12fed0965cf6f703d81f9
Alan <anochenson@gmail.com>
2013-11-06 19:15:17 +0000

    updates

--- createLook.php
+++ createLook.php
@@ -2,7 +2,4 @@
 
 include('functions.php');
 
-$sid = enter_new_look($_REQUEST['param_hitId']);
-
-header('Content-Type: application/json');
-die(json_encode(array('sid'=>$sid)));
+enter_new_look($_REQUEST);
