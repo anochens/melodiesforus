33fe17d1f417600f935a3b37606c425951e93f71
Alan Nochenson <anochenson@psu.edu>
2014-02-04 05:43:05 +0000

    Added config.sample.php

---
+++ config.sample.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+define('DB_USERNAME','');
+define('DB_PASSWORD','');
+
+
+
+//The name and password for an external email service
+//which sends emails from the MelodiesFor.us service.
+define('SAFEDELIVERY_UNAME',"");
+define('SAFEDELIVERY_PWD',"");
+
